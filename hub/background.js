89624c7abda5673a1effba26b0362c0882aa5dbd
Evan <evanaguttesrivelon@gmail.com>
2025-01-13 15:27:31 +0100

    hub page top left

--- hub/background.js
+++ hub/background.js
@@ -2,7 +2,7 @@ chrome.action.onClicked.addListener(async () => {
 	window = await chrome.windows.create({
 		url: 'hub.html',
 		type: 'panel', width: 440, height: 480,
-		top: 150,
-		left: 590,
+		top: 0,
+		left: 0,
 	 });
 });
